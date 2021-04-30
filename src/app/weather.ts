9ec7144354287ddcf0f9d23e8461a62b74bf92a0
Alexander Haeusler <a.haeusler@neusta.de>
2021-04-30 09:47:11 +0200

    first commit

---
+++ src/app/weather.ts
@@ -0,0 +1,13 @@
+export interface Weather {
+  temp: string;
+  tempunit: string;
+  hum: string;
+  wspeed: string;
+  wgust: string;
+  wgustTM: string;
+  TwgustTM: string;
+  avgbearing: string;
+  domwinddir: string;
+  press: string;
+  date: string;
+}
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Weather } from '../weather';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+
+  public gauge: Weather = {
+    temp: '0',
+    tempunit: '0',
+    hum: '0',
+    wspeed: '0',
+    wgust: '0',
+    wgustTM: '0',
+    TwgustTM: '0',
+    avgbearing: '0',
+    domwinddir: '0',
+    press: '0',
+    date: '0'
+  };
+  public CurrentDate;
+  myDate = new Date();
+
+
+  constructor(private http: HttpClient) {
+    this.CurrentDate = new Date();
+  }
+
+  ngOnInit(): void {
+    this.getData();
+    setInterval(() => {
+      this.getData();
+    }, 60000);
+  }
+
+  public goToLink(url: string): void{
+    window.open(url, '_blank');
+  }
+
+  getData(): void {
+    const realtimeGauge = '/fileadmin/cam/wetter/weather.php';
+    //const realtimeGauge = './assets/data.json';
+    this.http.get(realtimeGauge, {}).subscribe((data: Weather) => {
+      this.gauge = data;
+    },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log(`Backend returned: ${err.error.message}`);
+        } else {
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      });
+  }
+}
